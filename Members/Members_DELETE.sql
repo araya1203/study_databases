81c6733a5b05e1a45e1a9c87276af5a85c65f4ba
ARAJO <dddd2511u@gmail.com>
2023-06-07 15:57:57 +0900

    first commit

--- Members/Members_DELETE.sql
+++ Members/Members_DELETE.sql
@@ -1,2 +1,9 @@
---  DELETE FROM table_name WHERE condition;
-DELETE FROM 
+-- DELETE FROM table_name WHERE condition;
+DELETE FROM members_hobby
+WHERE MEMBERS_ID = 'M-01';
+
+DELETE FROM membewrs_login
+WHERE MEMBERS_ID = 'M-01';
+  
+DELETE FROM members
+WHERE NAME = '홍길동';
