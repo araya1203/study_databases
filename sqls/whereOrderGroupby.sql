01fd37314baaea7249904e47635508290c5cdbfb
ARAJO <dddd2511u@gmail.com>
2023-06-07 18:21:17 +0900

    first commit

--- sqls/whereOrderGroupby.sql
+++ sqls/whereOrderGroupby.sql
@@ -12,3 +12,32 @@ GROUP BY EmployeeID;
 -- COUNT(*)	EmployeeID
 -- 14	7
 -- 6	9
+
+
+-- Aliases
+SELECT COUNT(*) as EMPLOYEE_CNT,T_ORS.EmployeeID
+FROM Orders as T_ORS
+WHERE T_ORS.EmployeeID IN (7, 9)
+GROUP BY T_ORS.EmployeeID;
+
+
+-- Result:
+-- Number of Records: 2
+-- EMPLOYEE_CNT	EmployeeID
+-- 14	7
+-- 6	9
+
+
+SELECT COUNT(*) AS EMPLOYEE_CNT, T_OUTER_ORS.EmployeeID
+FROM (
+     SELECT T_INNER_ORS.*
+     FROM ORDERS AS T_INNER_ORS
+     WHERE T_INNER_ORS.EmployeeID IN (7,9)
+    ) AS T_OUTER_ORS
+GROUP BY T_OUTER_ORS.EmployeeID;
+
+-- Result:
+-- Number of Records: 2
+-- EMPLOYEE_CNT	EmployeeID
+-- 14	7
+-- 6	9
