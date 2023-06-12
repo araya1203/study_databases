8facf3651b7ecebe134ae0dfa0a4acfb70a9eb8e
ARAJO <dddd2511u@gmail.com>
2023-06-12 12:47:49 +0900

    first commit

--- Cars/ConnectDBs.java
+++ Cars/ConnectDBs.java
@@ -21,11 +21,57 @@ public class ConnectDBs {
                 System.out.println(resultSet.getString("COMPANY_ID") + resultSet.getString("COMPANY"));
             }
 
+            // SELECT COUNT(*) AS CNT FROM factorys;
+            query = "SELECT COUNT(*) AS CNT FROM factorys";
+            resultSet = statement.executeQuery(query);
+            int totalCount = 0;
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("CNT"));
+                totalCount = resultSet.getInt("CNT");
+            }
+
+            /*
+            INSERT INTO factorys
+            (COMPANY_ID, COMPANY) 
+            VALUE 
+            ('CAR-01', 'AUDI');
+            */
+
+
+
+
+            /*update factorys
+             SET COMPANY = '패러리'
+             WHERE COMPANY_ID = 'CAR-01' ;
+             DELETE FROM factorys
+             WHERE COMPANY_ID = 'CAR-01' ; */
+
+           // DATA 수정
+
+            String companyId = "CAR-01";
+            String company = "AUDI" ;
+            query = "INSERT INTO factorys " +
+                    "(COMPANY_ID, COMPANY) " +
+                    " VALUE " +
+                    "('"+companyId+"', '"+company+"') ";           
+            int count = statement.executeUpdate(query);
+
+            // query = " UPDATE factorys " + 
+            // "SET COMPANY = '페러리' " + 
+            // "WHERE COMPANY_ID = '"+companyId+"' ";
+            // count = statement.executeUpdate(query); // CAR-01 의 AUDI를 페러리로 변경
+          
+            // query = "delete FROM factorys " + 
+            // "WHERE COMPANY_ID = '"+companyId+"'";
+            // count = statement.executeUpdate(query); // CAR-01 삭제
+
+            
+            
             System.out.println();
         } catch (Exception e) {
             // TODO: handle exception
-              System.out.println(e.getMessage());
+            System.out.println(e.getMessage());
         }
-          System.out.println();
+        System.out.println();
     }
 }
